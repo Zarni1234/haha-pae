b965db0d23f74acb02cc63443775d0f019041421
Zarni123 <zarniredmi2021@gmail.com>
2021-10-19 10:55:09 +0630

    love

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 console.log(`Hello Par Naw`);
 console.log(`Nay Kaung Yae Lar`);
 console.log(`အရမ်းလွမ်းတာပဲသိလား`);
+
+function sheloveme(love) {
+  if (1500) {
+    for (let i = 0; i > 5; i++) {
+      console.log(`I love her Very Much ${i}`);
+    }
+  } else if (shehateme) {
+    console.log(`I go away form her`);
+  } else {
+    console.log(`I keep her in the deepest place of my heart`);
+  }
+}
+
+console.log(sheloveme(1500));
